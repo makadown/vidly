a27b00cd7f4f43751b627f38f9d664a4650ee85b
Mario Serrano <maseflores79@gmail.com>
2018-06-24 18:03:33 -0700

    Eager loading de membership

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -16,7 +18,7 @@ namespace Vidly.Controllers
         public ViewResult Index()
         {
             /*var customers = GetCustomers();*/
-            var customers = _context.Customers.ToList();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
 
